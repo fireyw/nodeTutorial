bd6cc2be03dfde4c953cb30f2a46ade84f9be3c3
kywsc <fireyw@gmail.com>
2018-04-25 15:03:26 +0900

    session coding

---
+++ app_session.js
@@ -0,0 +1,85 @@
+var express= require('express');
+var session= require('express-session');
+var bodyParser= require('body-parser');
+
+var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(urlencodedParser);
+//app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+    //cookie: { secure: true }
+}));
+
+app.get('/auth/logout', function(req, res){
+    delete req.session.displayName;  //session delete (js command)
+
+    res.send(`<h1>welcome</h1>
+                  <a href="/auth/login">login</a>  
+        `);
+});
+
+app.post('/auth/login', function(req, res){
+    var user= {
+        username:'fireyw',
+        pwd:'1111',
+        displayName:'용우동'
+    };
+
+    var uname=req.body.username;
+    var pwd= req.body.password;
+
+    if(uname==user.username && pwd==user.pwd){
+        req.session.displayName=user.displayName;
+        res.redirect('/welcome');
+    }else {
+        res.send('who are you? <a href="/auth/login">login</a>');
+    }
+});
+
+app.get('/welcome', function(req, res){
+    if(req.session.displayName){
+        res.send(`<h1>Hello ${req.session.displayName}</h1> 
+                  <a href="/auth/logout">logout</a>
+                  `);
+    }else{
+        res.send(`<h1>welcome</h1>
+                  <a href="/auth/login">login</a>  
+        `);
+    }
+});
+
+app.get('/auth/login', function(req, res){
+   var output=`
+   <form action='/auth/login' method='post'>
+   <p>
+        <input type='text' name='username' placeholder='username'>
+   </p>
+   <p>
+        <input type='password' name='password' placeholder="password">
+   </p>
+   <p>
+        <input type="submit">
+    </p>
+    </form>
+   `;
+   res.send(output);
+});
+
+app.get('/count', function(req, res){
+    if(req.session.count){
+        req.session.count++;
+    }else{
+        req.session.count=1
+    }
+   res.send('count : ' + req.session.count);
+});
+
+
+app.listen(3003, function(){
+    console.log('3003 port start');
+});
